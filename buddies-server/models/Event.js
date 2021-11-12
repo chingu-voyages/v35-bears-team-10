fb7499e0b6a173b3c194a274eed985b21d0314cd
Megumikawa <megumi19901016@gmail.com>
2021-11-12 14:45:30 +0100

    created event models

--- buddies-server/models/Event.js
+++ buddies-server/models/Event.js
@@ -1,13 +1,14 @@
 const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
 
 const EventSchema = new mongoose.Schema({
-  eventName:{
+  name:{
     type:String,
-    required: true,
+    // required: true,
   },
   date:{
     type: Date,
-    required: true
+    // required: true
   },
   picture: {
     type: String,
@@ -19,7 +20,7 @@ const EventSchema = new mongoose.Schema({
   activity: {
     type: String,
     enum:['drink','talk','walk','sports','coffee','party','boardgames', 'videogames'],
-    required: true,
+    // required: true,
   },
   // hostId: {
     // type: Schema.Types.ObjectId,
@@ -33,6 +34,10 @@ const EventSchema = new mongoose.Schema({
     type: Array,
     default: [],
   },
+  userId: {
+    type: String,
+    required:true
+    }
 },
   {timestamps: true}
 )
--- buddies-server/routes/event.js
+++ buddies-server/routes/event.js
@@ -1,10 +1,51 @@
 const Event = require("../models/Event");
+const User = require("../models/User")
 const router = require("express").Router();
 
 //create event
-router.get("/",(req, res) => {
- res.send("eventpage")
+router.post("/", async (req, res)=>{
+const newEvent = new Event(req.body)
+  try{
+    const savedEvent = await newEvent.save();
+    res.status(200).json(savedEvent)
+
+  }catch(err){
+    res.status(500).json(err)
+  }
 })
 
+// update event
+router.put("/:id", async(req, res) => {
+  try{
+    const event = await Event.findById(req.params.id)
+    if(event.userId === req.body.userId){
+      await event.updateOne({ $set: req.body })
+      res.status(200).json("you updated your event")
+    }else{
+      res.status(403).json("You can only update your own posts")
+    }
+  } catch(err) {
+    res.status(500).json(err)
+  }
+})
+
+// delet event
+router.delete("/:id", async (req, res) => {
+  
+    try {
+      
+      const event = await Event.findById(req.params.id)
+      
+    if(event.userId === req.body.userId){
+      await event.deleteOne()
+      res.status(200).json("event has been deleted");
+    } else {
+      res.status(403).json("you can only delete your own event");
+    }
+  }  catch (err) {
+      return res.status(500).json(err);
+    }
+    
+});
 
 module.exports = router;
