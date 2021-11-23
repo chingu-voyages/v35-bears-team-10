13fb57bcb7457c37f290736c0e5e2f5014a9533b
shafrazi <shafrazi@live.com>
2021-11-24 00:05:25 +0530

    add mapbox integration

--- buddies-client/src/App.js
+++ buddies-client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Home from "./components/Home";
+import Map from "./components/Map";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} exact />
         <Route path="/register" element={<Register />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </div>
   );
