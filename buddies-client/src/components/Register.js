117dd93b7f6dda10ba38f5c6d5344d32354fd614
John Binken <jcjbinken@gmail.com>
2021-11-26 15:16:54 +0200

    Registration and Login works again

--- buddies-client/src/components/Register.js
+++ buddies-client/src/components/Register.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import { useNavigate } from 'react-router-dom';
 function Register() {
   const initialValues = {
     username: "",
@@ -10,9 +9,6 @@ function Register() {
     password: "",
     confirmPassword: "",
   };
-
-  const navigate = useNavigate()
-
   const validationSchema = () => {
     return Yup.object().shape({
       username: Yup.string()
@@ -31,7 +27,6 @@ function Register() {
         .oneOf([Yup.ref("password"), null], "Confirm Password does not match"),
     });
   };
-
   const [serverState, setServerState] = useState();
       const handleServerResponse = (ok, msg) => {
         setServerState({ok, msg});
@@ -44,28 +39,23 @@ function Register() {
           
         })
           .then(response => {
-            navigate.push("/login");
+            
             actions.setSubmitting(false);
             actions.resetForm();
             handleServerResponse(true, "You are registered");
           })
           .catch(error => {
-            
+
             actions.setSubmitting(false);
+            handleServerResponse(false, error.response.data.error);
             handleServerResponse(false, error+"");
           });
       };
 
   return (
-    <div> <svg className="design" width="146" height="137" viewBox="0 0 146 137" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M82 37C82 92.2285 37.2285 137 -18 137C-73.2285 137 -118 92.2285 -118 37C-118 -18.2285 -73.2285 -63 -18 -63C37.2285 -63 82 -18.2285 82 37Z" fill="#D98C00" fill-opacity="0.71"/>
-    <circle cx="46" cy="-18" r="100" fill="#E9C441" fill-opacity="0.71"/>
-    </svg>
-    <h4 className="text-center">Welcome to Buddies!</h4>
-    <p className="desc-register">Let's have some fun!</p>
     <div className="row justify-content-center mt-5">
       <div className="col-6">
-        
+        <h4 className="text-center">Register</h4>
         <Formik
           initialValues={initialValues}
           validationSchema={validationSchema}
@@ -84,19 +74,17 @@ function Register() {
                   className="text-danger"
                 />
               </div>
-
               <div className="mb-3 col-12">
                 <label htmlFor="email" className="form-label">
                   Email address
                 </label>
-                <Field className="styleForm"name="email" type="email" className="form-control" />
+                <Field name="email" type="email" className="form-control" />
                 <ErrorMessage
                   name="email"
                   component="div"
                   className="text-danger"
                 />
               </div>
-
               <div className="mb-3 col-12">
                 <label htmlFor="password" className="form-label">
                   Password
@@ -113,7 +101,6 @@ function Register() {
                   className="text-danger"
                 />
               </div>
-
               <div className="mb-3 col-12">
                 <label htmlFor="confirmPassword" className="form-label">
                   Confirm password
@@ -129,7 +116,6 @@ function Register() {
                   className="text-danger"
                 />
               </div>
-
               <div className="form-group d-flex justify-content-center">
                 <button
                   type="submit"
@@ -139,10 +125,6 @@ function Register() {
                 >
                   Register
                 </button>
-                <div className="haveAccount">
-                  <p className="text-1">
-                    <span className="span0">Already have an account ?</span>
-                    <span className="span1">Sign in</span></p></div>
                 {serverState && (
                   <p className={!serverState.ok ? "errorMsg" : ""}>
                     {serverState.msg}
@@ -153,8 +135,7 @@ function Register() {
           )}
         </Formik>
       </div>
-    </div></div>
+    </div>
   );
 }
-
 export default Register;
