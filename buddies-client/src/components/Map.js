018a0fe94a4d49a18f77a37f8ca333cfc4ac2c69
shafrazi <shafrazi@live.com>
2021-11-28 16:49:43 +0530

    add react map gl and markers for events

--- buddies-client/src/components/Map.js
+++ buddies-client/src/components/Map.js
@@ -1,64 +1,67 @@
-import { useRef, useState, useEffect } from "react";
-import Marker from "./marker";
-import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import { useState, useMemo } from "react";
+import ReactMapGL, { GeolocateControl, Marker } from "react-map-gl";
+import party from "../images/party.png";
 
 const events = [
   {
     name: "Event 1",
     date: "2021-12-31",
-    location: ["80.4039", "6.6886"],
+    location: [80.4039, 6.6886],
     activity: "party",
     userId: "1",
   },
 ];
 
-mapboxgl.accessToken =
-  "pk.eyJ1Ijoic2hhZnJhemkiLCJhIjoiY2t3Y2V2cDd0MG9jZzJ1cWt0cTN4NjRrZyJ9.bAyu7dLgQogqZfn4SnzweQ";
+const geolocateControlStyle = {
+  right: 10,
+  top: 10,
+};
 
 export default function Map() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
-  const [zoom, setZoom] = useState(9);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [lng, lat],
-      zoom: zoom,
-    });
-
-    map.current.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        // When active the map will receive updates to the device's location as it changes.
-        // trackUserLocation: true,
-        // Draw an arrow next to the location dot to indicate which direction the device is heading.
-        showUserHeading: true,
-      })
-    );
+  const [viewport, setViewPort] = useState({
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 9,
   });
 
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on("move", () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
+  const markers = useMemo(
+    () =>
+      events.map((event) => (
+        <Marker
+          key={event.name}
+          longitude={event.location[0]}
+          latitude={event.location[1]}
+        >
+          <img
+            src={party}
+            alt="party"
+            width="40px"
+            height="40px"
+            onClick={() => {
+              console.log("hello");
+            }}
+          />
+        </Marker>
+      )),
+    [events]
+  );
 
   return (
-    <div>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
-    </div>
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={(viewport) => setViewPort(viewport)}
+      mapboxApiAccessToken="pk.eyJ1Ijoic2hhZnJhemkiLCJhIjoiY2t3Y2V2cDd0MG9jZzJ1cWt0cTN4NjRrZyJ9.bAyu7dLgQogqZfn4SnzweQ"
+      mapStyle="mapbox://styles/mapbox/streets-v11"
+      width="100%"
+      height="100vh"
+    >
+      <GeolocateControl
+        style={geolocateControlStyle}
+        positionOptions={{ enableHighAccuracy: true }}
+        trackUserLocation={true}
+        auto
+      />
+      {markers}
+    </ReactMapGL>
   );
 }
--- buddies-client/src/components/marker.js
+++
@@ -1,3 +0,0 @@
-export default function Marker() {
-  return <div className="marker"></div>;
-}
