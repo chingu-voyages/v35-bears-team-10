e490ae381e2e8ac682b922242e561c59f0262cb6
shafrazi <shafrazi@live.com>
2021-12-16 02:09:49 +0530

    minor design changes to popup

--- buddies-client/src/components/AddEventForm.js
+++ buddies-client/src/components/AddEventForm.js
@@ -1,15 +1,15 @@
 export default function AddEventForm({ isOpen }) {
   return (
     <div
-      className="bg-white z-10 absolute w-1/2 md:w-1/3 px-3 py-4 flex flex-col justify-center items-center"
+      className="z-10 absolute w-full md:w-1/3 px-3 py-4 flex flex-col justify-center items-center"
       style={{ display: !isOpen && "none" }}
     >
-      <div className="">
-        <h1 className="font-bold text-lg">What's happening?</h1>
+      <div className="px-3 py-4 bg-white w-full flex flex-col justify-center items-center rounded">
+        <h1 className="font-bold text-lg text-center">What's happening?</h1>
         <input
           type="text"
           placeholder="Date"
-          className="bg-gray-200 px-3 py-1 rounded"
+          className="bg-gray-200 px-3 py-1 rounded mt-2"
         ></input>
       </div>
     </div>
