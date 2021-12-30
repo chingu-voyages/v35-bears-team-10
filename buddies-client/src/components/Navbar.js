702ec644251d63d394066f55bf05dda89d0b1c66
John Binken <jcjbinken@gmail.com>
2021-12-30 15:47:54 +0200

    hello

--- buddies-client/src/components/Navbar.js
+++ buddies-client/src/components/Navbar.js
@@ -40,12 +40,8 @@ function Navbar() {
             </div>
             <div className="flex-1 md:flex md:items-center md:justify-between hidden">
               <div className="flex flex-col -mx-4 md:flex-row md:items-center md:mx-8">
-                <a
-                  href="/"
-                  className="px-2 mx-2  text-sm font-medium text-gray-700 transition-colors duration-200 transform rounded-md md:mt-0 dark:text-gray-200 hover:bg-gray-300 dark:hover:bg-gray-700"
-                >
-                  About
-                </a>
+              
+                
               </div>
               <div className="flex items-center mt-1 md:mt-0">
                 {isLoggedIn ? (
--- buddies-client/src/components/Profile.js
+++ buddies-client/src/components/Profile.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState} from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import ProfileBgImg from "../images/profile-bg.png";
@@ -7,7 +7,28 @@ import { UserContext } from "../context/user-context";
 function Profile() {
   const { user, isLoggedIn } = useContext(UserContext);
   const location = useLocation();
+  const [image, setImage] = useState('')
+  const [loading, setLoading] = useState(false)
 
+ 
+  const uploadImage = async e =>{
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0])
+    data.append('upload_preset', 'buddies')
+    setLoading(true)
+    const res = await fetch(
+     'https://api.cloudinary.com/v1_1/dwmf1yie2/image/upload' , {
+       method:"POST",
+       body: data
+     }
+    )
+     const file = await res.json()
+
+     setImage(file.secure_url)
+     setLoading(false)
+   }
+ 
   if (!isLoggedIn) {
     return <Navigate to="/login" state={{ from: location }} />;
   }
@@ -23,6 +44,18 @@ function Profile() {
           />
           <div className="flex flex-col justify-center items-center w-full">
             <h3 className="font-bold text-lg">Edit Profile</h3>
+            
+      <input
+        type="file"
+        name="file"
+        placeholder="Upload an image"
+        onChange={uploadImage}
+      />
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <img src={image} alt="uploadimage" style={{ width: '300px' }} />
+      )}
             {user && (
               <form className="loginBox m-6">
                 <input
