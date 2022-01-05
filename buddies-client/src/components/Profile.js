6fc053688ceb49b2f3efb9942f207aa6a96ebbe3
John Binken <jcjbinken@gmail.com>
2022-01-05 13:52:58 +0200

    Cloudinary works locally need to send it to the database

--- buddies-client/src/components/Profile.js
+++ buddies-client/src/components/Profile.js
@@ -1,20 +1,69 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+
 import { Navigate, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
-
+import ProfileBgImg from "../images/profile-bg.png";
+import Upload from "../components/Upload";
 import { UserContext } from "../context/user-context";
 
 function Profile() {
-  const { user, isLoggedIn } = useContext(UserContext);
+  const { user } = useContext(UserContext);
   const location = useLocation();
 
-  if (!isLoggedIn) {
+  if (!user) {
     return <Navigate to="/login" state={{ from: location }} />;
   }
+
   return (
     <div className="">
       <Navbar />
-      
+      <div className="profile">
+        <div className="flex flex-col md:flex-row w-full">
+          <img
+            src={ProfileBgImg}
+            alt="profile-bg"
+            className="hidden md:flex md:w-1/3 md:h-full md:object-cover"
+          />
+          <div className="flex flex-col justify-center items-center w-full">
+            <h3 className="font-bold text-lg">Edit Profile</h3>
+            {user && (
+              <form className="loginBox m-6">
+                <input
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  id="loginInput"
+                  value={user.username}
+                />
+
+                <input
+                  placeholder="Email"
+                  type="email"
+                  required
+                  className="loginInput"
+                  value={user.email}
+                />
+
+                <input
+                  placeholder="Password"
+                  type="password"
+                  required
+                  minLength="6"
+                  className="loginInput"
+                />
+
+                <button className="loginButton" type="submit">
+                  Save
+                </button>
+              </form>
+            )}
+
+
+
+                <Upload/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ buddies-client/src/components/Upload.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+const Upload = () => {
+const [image, setImage ] = useState("");
+const [ url, setUrl ] = useState("");
+const uploadImage = () => {
+const data = new FormData()
+data.append("file", image)
+data.append("upload_preset", "buddies")
+data.append("cloud_name","dwmf1yie2")
+fetch("  https://api.cloudinary.com/v1_1/dwmf1yie2/image/upload",{
+method:"post",
+body: data
+})
+.then(resp => resp.json())
+.then(data => {
+setUrl(data.url)
+
+})
+.catch(err => console.log(err))
+}
+console.log(url)
+return (
+<div>
+<div>
+<input type="file" onChange= {(e)=> setImage(e.target.files[0])}></input>
+<button onClick={uploadImage}>Upload</button>
+</div>
+<div>
+<h1>Uploaded image will be displayed here</h1>
+<img src={url} alt="userImg"/>
+</div>
+</div>
+)
+}
+export default Upload;
